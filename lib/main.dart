66647cabf3993a6e655bd0922cdd934910838dda
alexandr <alexander.mischenko@bask.ws>
2023-07-25 16:30:30 +0500

    default zoom options has been added

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -21,16 +20,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -38,21 +27,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: FlutterMap(
           options: MapOptions(
-            center: const LatLng(51.509364, -0.128928),
-            zoom: 9.2,
-          ),
+              center: const LatLng(54, 61), zoom: 9.2, maxZoom: 18, minZoom: 1),
           nonRotatedChildren: [
             RichAttributionWidget(
               attributions: [
@@ -66,8 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
           children: [
             TileLayer(
-              urlTemplate:
-                  'https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
               userAgentPackageName: 'com.example.app',
             ),
           ],
